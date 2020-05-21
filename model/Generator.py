f599152b84cdfa00fe9ea233eb4d3f167221aa1d
boaloysius <boaloysius@gmail.com>
2020-05-21 16:31:47 +0000

    added torch.nn to all module files

---
+++ model/Generator.py
@@ -0,0 +1,15 @@
+from model.EncodeDecoder import DownSampleModule, UpSampleModule
+import torch.nn as nn
+
+class Generator(nn.Module):
+
+    def __init__(self, nc_in, nc_out, nf):
+        super().__init__()
+        self.downsample_module = DownSampleModule(nc_in, nf)
+        self.upsample_module = UpSampleModule(nf * 4, nc_out, nf)
+
+
+    def forward(self, masked_imgs, masks, guidances=None):
+        encoded_features = self.downsample_module(inp)
+        c11 = self.upsample_module(encoded_features)
+        return out
