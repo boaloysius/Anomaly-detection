5311521a3005f026f1495ef17b2e770752bc03ec
boaloysius <boaloysius@gmail.com>
2020-09-16 13:35:06 +0000

    2D BN

--- model/blocks.py
+++ model/blocks.py
@@ -9,13 +9,14 @@ class NN3Dby2D(nn.Module):
         super().__init__()
 
         self.layer = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding)
-        self.layer = nn.utils.spectral_norm(self.layer)
+        #self.layer = nn.utils.spectral_norm(self.layer)
+        self.bn = nn.BatchNorm2d(out_channels, affine=False)
         self.activation = activation
 
     def forward(self, xs):
         xs = torch.unbind(xs, dim=2) # [B, C, L, H, W]
         # Unbind the video data to a tuple of frames
-        xs = torch.stack([self.layer(x) for x in xs], dim=2)
+        xs = torch.stack([self.bn(self.layer(x)) for x in xs], dim=2)
 
         if self.activation:
             xs = self.activation(xs)
