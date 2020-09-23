4f9bd719009ee2c32e9bcbe6a86db08048522a2c
boaloysius <boaloysius@gmail.com>
2020-09-23 11:31:42 +0000

    Generator UNet

--- model/UNet_2D.py
+++ model/UNet_2D.py
@@ -34,20 +34,30 @@ class DownSampleModule(nn.Module):
         return F.interpolate(c, scale_factor=(1, sf, sf))
 
     def forward(self, inp):
-        out = [inp]
-        out.append(self.conv1(out[-1]))
-        out.append(self.conv2(out[-1]))
-        out.append(self.conv3(self.interpolate(out[-1],1/2)))
-        out.append(self.conv4(out[-1]))
+        saved = []
+        out = inp
+        out = self.conv1(out)
+        out = self.conv2(out)
+        saved.append(out)
+
+        out = self.conv3(self.interpolate(out,1/2))
+        out = self.conv4(out)
+        saved.append(out)
         #'''
-        out.append(self.conv5(self.interpolate(out[-1],1/2)))
-        out.append(self.conv6(out[-1]))
-        out.append(self.conv7(self.interpolate(out[-1],1/2)))
-        out.append(self.conv8(out[-1]))
-        out.append(self.conv9(self.interpolate(out[-1],1/2)))
-        out.append(self.conv10(out[-1]))
-        out.append(self.conv11(self.interpolate(out[-1],1/2)))
-        out.append(self.conv12(out[-1]))
+        out = self.conv5(self.interpolate(out,1/2))
+        out = self.conv6(out)
+        saved.append(out)
+
+        out = self.conv7(self.interpolate(out,1/2))
+        out = self.conv8(out)
+        saved.append(out)
+
+        out = self.conv9(self.interpolate(out,1/2))
+        out = self.conv10(out)
+        saved.append(out)
+
+        out = self.conv11(self.interpolate(out,1/2))
+        out = self.conv12(out)
         #'''
         return out
 
@@ -77,19 +87,23 @@ class UpSampleModule(nn.Module):
     def interpolate(self, c, sf):
         return F.interpolate(c, scale_factor=(1, sf, sf))
 
-    def forward(self, inp):
+    def concat(self, x_down, x_up):
+
+      torch.cat([x_up, x_down], dim=1)
+
+    def forward(self, inp, saved):
         out = inp
         #'''
-        out = self.conv1(self.interpolate(out,2))
+        out = self.conv1(self.concat(saved[-1] ,self.interpolate(out,2)))
         out = self.conv2(out)
-        out = self.conv3(self.interpolate(out,2))
+        out = self.conv3(self.concat( pervious,self.interpolate(out,2)))
         out = self.conv4(out)
-        out = self.conv5(self.interpolate(out,2))
+        out = self.conv5(self.concat( pervious,self.interpolate(out,2)))
         out = self.conv6(out)
-        out = self.conv7(self.interpolate(out,2))
+        out = self.conv7(self.concat( pervious,self.interpolate(out,2)))
         out = self.conv8(out)
         #'''
-        out = self.conv9(self.interpolate(out,2))
+        out = self.conv9(self.concat( pervious,self.interpolate(out,2)))
         out = self.conv10(out)
         out = self.conv11(out)
         out = self.conv12(out)
