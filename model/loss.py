1fc09603da8a0cfc65a19a9ce27d9cb22bd8d36e
boaloysius <boaloysius@gmail.com>
2020-09-27 18:35:17 +0000

    edge loss

--- model/loss.py
+++ model/loss.py
@@ -1,10 +1,14 @@
 import os
 import sys
 import torch
+import torch.nn as nn
+
+from utils import *
 
 from libs.CannyEdgePytorch.net_canny import Net as CannyEdgeNet
 
-device = torch.device("cuda")
+device = device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
 canny_edge_net = CannyEdgeNet(threshold=2.0, use_cuda=True).to(device)
 canny_edge_net.eval()
 
@@ -21,34 +25,33 @@ class EdgeLoss(nn.Module):
         self.l1_loss = nn.L1Loss()
 
     def edge_loss(self, output, target):
-        from utils.edge import get_edge
+
+        #print(output.shape, target.shape)
+        #view_img([
+        #          tanh2sigmoid(output.to("cpu"))[0],
+        #          tanh2sigmoid(target.to("cpu"))[0]
+        #          ])
+
         output_edge = get_edge(output)
         gt_edge = get_edge(target)
         loss = self.l1_loss(output_edge, gt_edge)
-        return loss, output_edge, gt_edge
+        return loss
 
     def forward(self, data_input, model_output):
-        targets = data_input['targets']
-        outputs = model_output['outputs']
+        targets = data_input
+        outputs = model_output
 
         mean_image_loss = []
         output_edges = []
         target_edges = []
-        for batch_idx in range(targets.size(0)):
+        for batch_idx in range(len(targets)):
             edges_o = []
             edges_t = []
-            for frame_idx in range(targets.size(1)):
-                loss, output_edge, target_edge = self.edge_loss(
-                    outputs[batch_idx, frame_idx:frame_idx + 1],
-                    targets[batch_idx, frame_idx:frame_idx + 1]
+            for frame_idx in range(targets[0].size(0)):
+                loss = self.edge_loss(
+                    outputs[batch_idx][frame_idx:frame_idx + 1],
+                    targets[batch_idx][frame_idx:frame_idx + 1]
                 )
                 mean_image_loss.append(loss)
-                edges_o.append(output_edge)
-                edges_t.append(target_edge)
-            output_edges.append(torch.cat(edges_o, dim=0))
-            target_edges.append(torch.cat(edges_t, dim=0))
 
-        mean_image_loss = torch.stack(mean_image_loss, dim=0).mean(dim=0)
-        self.current_output_edges = output_edges
-        self.current_target_edges = target_edges
-        return mean_image_loss
+        return torch.stack(mean_image_loss, dim=0).mean(dim=0)
