c1ff10b1f4fd9cb4d818c0887a5e4b102e5a3c11
boaloysius <boaloysius@gmail.com>
2020-09-29 17:14:20 +0000

    Residual

--- model/EncodeDecoder_TSM.py
+++ model/EncodeDecoder_TSM.py
@@ -3,44 +3,42 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class DownSampleModule(nn.Module):
-    def __init__(self, nc_in, nf):
+    def __init__(self, nc_in, nf, residual=True):
         super().__init__()
+
+        self.residual = residual
 
         #Input 3-64
         self.conv1 = NN3Dby2D(nc_in, nf * 1, kernel_size=(3, 3), stride=1,padding=1)
         self.conv2 = NN3Dby2D(nf * 1, nf * 1, kernel_size=(3, 3), stride=1,padding=1)
 
         # Downsample 64-128
-        self.conv3 = NN3Dby2DTSM(nf * 1, nf * 2, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
+        self.conv3 = NN3Dby2D(nf * 1, nf * 2, kernel_size=(3, 3), stride=(1, 1), padding=(1, 1))
         self.conv4 = NN3Dby2DTSM(nf * 2, nf * 2, kernel_size=(3, 3), stride=(1, 1), padding=1)
         
         # Downsample 128-256
-        self.conv5 = NN3Dby2DTSM(nf * 2, nf * 4, kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.conv5 = NN3Dby2D(nf * 2, nf * 4, kernel_size=(3, 3), stride=(1, 1), padding=1)
         self.conv6 = NN3Dby2DTSM(nf * 4, nf * 4, kernel_size=(3, 3), stride=(1, 1), padding=1)
 
         # Downsample 256-512
-        self.conv7 = NN3Dby2DTSM(nf * 4, nf * 8, kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.conv7 = NN3Dby2D(nf * 4, nf * 8, kernel_size=(3, 3), stride=(1, 1), padding=1)
         self.conv8 = NN3Dby2DTSM(nf * 8, nf * 8, kernel_size=(3, 3), stride=(1, 1), padding=1)
 
         # Downsample 512-1024
-        self.conv9 = NN3Dby2DTSM(nf * 8, nf * 16, kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.conv9 = NN3Dby2D(nf * 8, nf * 16, kernel_size=(3, 3), stride=(1, 1), padding=1)
         self.conv10 = NN3Dby2DTSM(nf * 16, nf * 16, kernel_size=(3, 3), stride=(1, 1), padding=1)
 
         # Downsample 1024-2048
-        self.conv11 = NN3Dby2DTSM(nf * 16, nf * 32, kernel_size=(3, 3), stride=(1, 1), padding=1)
+        self.conv11 = NN3Dby2D(nf * 16, nf * 32, kernel_size=(3, 3), stride=(1, 1), padding=1)
         self.conv12 = NN3Dby2DTSM(nf * 32, nf * 32, kernel_size=(3, 3), stride=(1, 1), padding=1)
 
     def interpolate(self, c, sf):
         return F.interpolate(c, scale_factor=(1, sf, sf))
 
-    def residual_TSM(self, layer, x_in, residual=True):
-      x_conv = layer(x_in)
-      if(residual):
-        x_in_split   = torch.unbind(x_in, dim=2) # [B, C, L, H, W]
-        x_conv_split = torch.unbind(x_conv, dim=2) # [B, C, L, H, W]
-        xs = [torch.cat([x_in_split[i], x_conv_split[i]], dim=1) for i in range(len(x_in_split))]
-        xs = torch.stack([x for x in xs], dim=2)
-        return xs
+    def residual_TSM(self, layer, identity):
+      x_conv = layer(identity)
+      if(self.residual):
+        return x_conv + identity
       else:
         return x_conv
 
@@ -50,45 +48,48 @@ class DownSampleModule(nn.Module):
         out = self.conv2(out)
         
         out = self.interpolate(out,1/2)
-        out = self.residual_TSM(self.conv3, out, self.residual)
-        out = self.residual_TSM(self.conv4, out, self.residual)
+        out = self.conv3(out)
+        out = self.residual_TSM(self.conv4, out)
         
         out = self.interpolate(out,1/2)
-        out = self.residual_TSM(self.conv5, out, self.residual)
-        out = self.residual_TSM(self.conv6, out, self.residual)
+        out = self.conv5(out)
+        out = self.residual_TSM(self.conv6, out)
         
         out = self.interpolate(out,1/2)
-        out = self.residual_TSM(self.conv7, out, self.residual)
-        out = self.residual_TSM(self.conv8, out, self.residual)
+        out = self.conv7(out)
+        out = self.residual_TSM(self.conv8, out)
 
         out = self.interpolate(out,1/2)
-        out = self.residual_TSM(self.conv9, out, self.residual)
-        out = self.residual_TSM(self.conv10, out, self.residual)
+        out = self.conv9(out)
+        out = self.residual_TSM(self.conv10, out)
         
         out = self.interpolate(out,1/2)
-        out = self.residual_TSM(self.conv11, out, self.residual)
-        out = self.residual_TSM(self.conv12, out, self.residual)
+        out = self.conv11(out)
+        out = self.residual_TSM(self.conv12, out)
         #'''
         return out
 
 
 class UpSampleModule(nn.Module):
-    def __init__(self, nc_out, nf):
+    def __init__(self, nc_out, nf, residual=True):
         super().__init__()
+
+        self.residual = residual
+
         # Upsample 2048-1024
-        self.conv1 = NN3Dby2DTSM(nf*32, nf*16, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv1 = NN3Dby2D(nf*32, nf*16, kernel_size=(3, 3), stride=1, padding=1)
         self.conv2 = NN3Dby2DTSM(nf*16, nf*16, kernel_size=(3, 3), stride=(1, 1), padding=1)
         # Upsample 1024-512
-        self.conv3 = NN3Dby2DTSM(nf*16, nf*8, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv3 = NN3Dby2D(nf*16, nf*8, kernel_size=(3, 3), stride=1, padding=1)
         self.conv4 = NN3Dby2DTSM(nf*8, nf*8, kernel_size=(3, 3), stride=(1, 1), padding=1)
         # Upsample 512-256
-        self.conv5 = NN3Dby2DTSM(nf*8, nf*4, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv5 = NN3Dby2D(nf*8, nf*4, kernel_size=(3, 3), stride=1, padding=1)
         self.conv6  = NN3Dby2DTSM(nf*4, nf*4, kernel_size=(3, 3), stride=(1, 1), padding=1)
         # Upsample 256-128 
-        self.conv7 = NN3Dby2DTSM(nf*4, nf*2, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv7 = NN3Dby2D(nf*4, nf*2, kernel_size=(3, 3), stride=1, padding=1)
         self.conv8 = NN3Dby2DTSM(nf*2, nf*2, kernel_size=(3, 3), stride=(1, 1), padding=1)
         # Upsample 128-64
-        self.conv9 = NN3Dby2DTSM(nf*2, nf*1, kernel_size=(3, 3), stride=1, padding=1)
+        self.conv9 = NN3Dby2D(nf*2, nf*1, kernel_size=(3, 3), stride=1, padding=1)
         self.conv10 = NN3Dby2DTSM(nf*1, nf*1, kernel_size=(3, 3), stride=(1, 1), padding=1)
         # Output
         self.conv11 = NN3Dby2D(nf*1, nf*1, kernel_size=(3, 3), stride=1, padding=1)
@@ -97,14 +98,10 @@ class UpSampleModule(nn.Module):
     def interpolate(self, c, sf):
         return F.interpolate(c, scale_factor=(1, sf, sf))
 
-    def residual_TSM(self, layer, x_in, residual=True):
-      x_conv = layer(x_in)
-      if(residual):
-        x_in_split   = torch.unbind(x_in, dim=2) # [B, C, L, H, W]
-        x_conv_split = torch.unbind(x_conv, dim=2) # [B, C, L, H, W]
-        xs = [torch.cat([x_in_split[i], x_conv_split[i]], dim=1) for i in range(len(x_in_split))]
-        xs = torch.stack([x for x in xs], dim=2)
-        return xs
+    def residual_TSM(self, layer, identity):
+      x_conv = layer(identity)
+      if(self.residual):
+        return x_conv + identity
       else:
         return x_conv
 
@@ -112,21 +109,25 @@ class UpSampleModule(nn.Module):
         out = inp
         #'''
         out = self.interpolate(out,2)
-        out = self.residual_TSM(self.conv1, out, self.residual)
-        out = self.residual_TSM(self.conv2, out, self.residual)
+        out = self.conv1(out)
+        out = self.residual_TSM(self.conv2, out)
 
         out = self.interpolate(out,2)
-        out = self.residual_TSM(self.conv3, out, self.residual)
-        out = self.residual_TSM(self.conv4, out, self.residual)
+        out = self.conv3(out)
+        out = self.residual_TSM(self.conv4, out)
+
         out = self.interpolate(out,2)
-        out = self.residual_TSM(self.conv5, out, self.residual)
-        out = self.residual_TSM(self.conv6, out, self.residual)
+        out = self.conv5(out)
+        out = self.residual_TSM(self.conv6, out)
+
         out = self.interpolate(out,2)
-        out = self.residual_TSM(self.conv7, out, self.residual)
-        out = self.residual_TSM(self.conv8, out, self.residual)
+        out = self.conv7(out)
+        out = self.residual_TSM(self.conv8, out)
+
         out = self.interpolate(out,2)
-        out = self.residual_TSM(self.conv9, out, self.residual)
-        out = self.residual_TSM(self.conv10, out, self.residual)
+        out = self.conv9(out)
+        out = self.residual_TSM(self.conv10, out)
+        
         out = self.conv11(out)
         out = self.conv12(out)
         return out
--- model/Generator_TSM.py
+++ model/Generator_TSM.py
@@ -5,8 +5,8 @@ class Generator(nn.Module):
 
     def __init__(self, nc_in, nc_out, nf):
         super().__init__()
-        self.downsample_module = DownSampleModule(nc_in, nf)
-        self.upsample_module = UpSampleModule(nc_out, nf)
+        self.downsample_module = DownSampleModule(nc_in, nf, residual = True)
+        self.upsample_module = UpSampleModule(nc_out, nf, residual = True)
 
 
     def forward(self, inp, guidances=None):
