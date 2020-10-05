65f56073373e68dbcf973b355d2923eb73144c6f
boaloysius <boaloysius@gmail.com>
2020-10-05 15:12:59 +0000

    sf

--- model/EncodeDecoder_TSM.py
+++ model/EncodeDecoder_TSM.py
@@ -32,8 +32,8 @@ class DownSampleModule(nn.Module):
         self.conv11 = NN3Dby2D(nf * 16, nf * 32, kernel_size=(3, 3), stride=(1, 1), padding=1)
         self.conv12 = NN3Dby2DTSM(nf * 32, nf * 32, kernel_size=(3, 3), stride=(1, 1), padding=1)
 
-    def interpolate(self, c, sf):
-        return F.interpolate(c, scale_factor=(1, sf, sf))
+    def interpolate(self, c, sf1=1, sf2=1, sf3=1):
+        return F.interpolate(c, scale_factor=(sf1, sf2, sf3))
 
     def residual_TSM(self, layer, identity):
       x_conv = layer(identity)
@@ -47,23 +47,23 @@ class DownSampleModule(nn.Module):
         out = self.conv1(out)
         out = self.conv2(out)
         
-        out = self.interpolate(out,1/2)
+        out = self.interpolate(out, sf2=1/2, sf3=1/2)
         out = self.conv3(out)
         out = self.residual_TSM(self.conv4, out)
         
-        out = self.interpolate(out,1/2)
+        out = self.interpolate(out, sf2=1/2, sf3=1/2)
         out = self.conv5(out)
         out = self.residual_TSM(self.conv6, out)
         
-        out = self.interpolate(out,1/2)
+        out = self.interpolate(out, sf2=1/2, sf3=1/2)
         out = self.conv7(out)
         out = self.residual_TSM(self.conv8, out)
 
-        out = self.interpolate(out,1/2)
+        out = self.interpolate(out, sf2=1/2, sf3=1/2)
         out = self.conv9(out)
         out = self.residual_TSM(self.conv10, out)
         
-        out = self.interpolate(out,1/2)
+        out = self.interpolate(out, sf2=1/2, sf3=1/2)
         out = self.conv11(out)
         out = self.residual_TSM(self.conv12, out)
         #'''
@@ -95,8 +95,8 @@ class UpSampleModule(nn.Module):
         self.conv11 = NN3Dby2D(nf*1, nf*1, kernel_size=(3, 3), stride=1, padding=1)
         self.conv12 = NN3Dby2D(nf*1, nc_out, kernel_size=(3, 3), stride=(1, 1), padding=1, bn=False, activation=nn.Tanh())
 
-    def interpolate(self, c, sf):
-        return F.interpolate(c, scale_factor=(1, sf, sf))
+    def interpolate(self, c, sf1=1, sf2=1, sf3=1):
+        return F.interpolate(c, scale_factor=(sf1, sf2, sf3))
 
     def residual_TSM(self, layer, identity):
       x_conv = layer(identity)
@@ -108,23 +108,23 @@ class UpSampleModule(nn.Module):
     def forward(self, inp):
         out = inp
         #'''
-        out = self.interpolate(out,2)
+        out = self.interpolate(out,sf2=2,sf3=2)
         out = self.conv1(out)
         out = self.residual_TSM(self.conv2, out)
 
-        out = self.interpolate(out,2)
+        out = self.interpolate(out,sf2=2,sf3=2)
         out = self.conv3(out)
         out = self.residual_TSM(self.conv4, out)
 
-        out = self.interpolate(out,2)
+        out = self.interpolate(out,sf2=2,sf3=2)
         out = self.conv5(out)
         out = self.residual_TSM(self.conv6, out)
 
-        out = self.interpolate(out,2)
+        out = self.interpolate(out,sf2=2,sf3=2)
         out = self.conv7(out)
         out = self.residual_TSM(self.conv8, out)
 
-        out = self.interpolate(out,2)
+        out = self.interpolate(out,sf2=2,sf3=2)
         out = self.conv9(out)
         out = self.residual_TSM(self.conv10, out)
         
